c56b090dc9a4b84d80a5a72d26409cd7c2fe148e
Brock Whitten <brock@chloi.io>
2012-09-09 14:11:34 -0700

    revives authenticate method

--- models/account.js
+++ models/account.js
@@ -5,17 +5,6 @@ var hash        = require("../lib/hash")
 
 module.exports = function(client) {
 
-  var email_verified = {
-    exist: function(field, obj, errors, next){
-      if(hasOwnProperty(field)){
-        next()
-      }else{
-        errors.push("must be true")
-        next()
-      }
-    } 
-  }
-
   var account = new Thug({
     "locals": {
       "namespace": "account",
@@ -143,35 +132,25 @@ module.exports = function(client) {
   
   account.constructor.prototype.authenticate = function(q, password, cb){
     var that = this
-    
     that.read(q, function(obj){
-      if(obj){
-        if(hash.validate(obj.hash, password)){
-          var ts = (new Date()).toJSON()
-          client.multi()
-          .hincrby(namespace +":" + obj.id, "login_count", 1)
-          .hset(namespace + ":" + obj.id, "login_at", ts)
-          .rpush(namespace + ":" + obj.id + ":logins", ts)
-          .ltrim(namespace + ":" + obj.id + ":logins", -100, -1)
-          .exec(function(err, replies){
-            that.get(obj.id, function(record){
-              cb(null, record)
-            })
-          })
-        }else{
-          var errors = {
-            details: {"password": "is not correct"},
-            messages: ["password is not correct"]
-          }
-          cb(errors, null)
-        }
-      }else{
-        var errors = {
+      if(!obj){
+        return cb({
           details: {"account": "is not in the system"},
           messages: ["account is not in the system"]
-        }
-        cb(errors, null)
+        }, null)
       }
+      hash.validate(password, obj.hash, function(err, rsp){
+        if(rsp === true){
+          that.get(obj.id, function(record){
+            cb(null, record)
+          })
+        }else{
+          cb({
+            details: {"password": "is not correct"},
+            messages: ["password is not correct"]
+          }, null)
+        }
+      })
     })
   }
   
