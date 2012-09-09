0abc519bac5f40d1a791328a6084c2f89efb14a5
Brock Whitten <brock@chloi.io>
2012-09-09 13:27:03 -0700

    bumped up security tests

--- test/security.js
+++ test/security.js
@@ -14,11 +14,28 @@ describe("security", function(){
     })
   })
 
+  it("should not be able to change id", function(done) {
+    rolodex.account.set({ email: "brock@sintaxi.com"}, { id: "567" }, function(errors, account){
+      account.should.have.property("id")
+      account.id.should.not.eql("567")
+      done()
+    })
+  })
+
   it("should not be able to change uuid", function(done) {
     rolodex.account.set({ email: "brock@sintaxi.com"}, { "uuid": "12345" }, function(errors, account){
       account.should.have.property("id")
       account.should.have.property("uuid")
       account.uuid.should.not.eql("12345")
+      done()
+    })
+  })
+
+  it("should return hash, password, password_confirmation propertied", function(done) {
+    rolodex.account.get({ email: "brock@sintaxi.com"}, function(account){
+      account.should.not.have.property("hash")
+      account.should.not.have.property("password")
+      account.should.not.have.property("password_confirmation")
       done()
     })
   })
