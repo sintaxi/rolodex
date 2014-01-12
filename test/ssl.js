6b45c2b4c5d50746d9cd25a8c668684dfed2c1da
Brock Whitten <brock@chloi.io>
2014-01-12 07:33:50 -0800

    remote calls working

--- test/ssl.js
+++ test/ssl.js
@@ -1,18 +1,19 @@
-// var fs     = require("fs")
+if(process.env.ROLE == "master"){
+  var fs     = require("fs")
+  var config  = require(__dirname + "/config/master.json")
 
-// var role   = process.env.ROLE || "master"
-// var config  = JSON.parse(fs.readFileSync(__dirname + "/config/"+ role +".json"))
+  config.ssl = {
+    "cert": __dirname + '/config/certificate.pem',
+    "key": __dirname + '/config/privatekey.pem'
+  };
 
-// config.ssl = {
-//   "cert": __dirname + '/config/certificate.pem',
-//   "key": __dirname + '/config/privatekey.pem'
-// };
+  describe("ssl", function(){
 
-// describe("ssl", function(){
+    it('should initialize with cert and key in config', function(done) {
+      require("../")(config).listen(8025, function(){
+        done()
+      });
+    });
 
-//   it('should initialize with cert and key in config', function(done) {
-//     require("../")(config).listen(8025, done);
-//   });
-
-// })
-
+  })
+}
